e29bad2f70997d2ffd28f52271c47f2a338be194
mieraldudhagara02 <118159123+mieraldudhagara02@users.noreply.github.com>
2024-07-02 22:57:24 +0530

    COMMIT

---
+++ PRACTICE/01.CORE_JAVA/10.RelationalOperators.java
@@ -0,0 +1,30 @@
+package operators_practice;
+
+public class RelationalOperators {
+	
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 4;
+		
+		//Equals to
+		System.out.println("a Equals To b : "+(a==b));
+		
+		//Not Equals To
+		System.out.println("a is Not Equals To b : "+(a!=b));
+		
+		//GreaterThan
+		System.out.println("b is GreaterThan a : "+(b>a));
+		
+		//LessThan
+		System.out.println("a is LessThan b : "+(a<b));
+		
+		//GreaterThan or Equals To
+		System.out.println("b is GreaterThan or Equals To a : "+(b>=a));
+		
+		//LessThan or Equals To
+		System.out.println("a is LessThan or Equals To b : "+(a<=b));
+		
+	}
+
+}
