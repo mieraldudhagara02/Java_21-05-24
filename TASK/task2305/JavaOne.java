6950e193ebd12302317025433459e8f8789a426f
mieraldudhagara02 <118159123+mieraldudhagara02@users.noreply.github.com>
2024-07-02 23:26:05 +0530

    commit

--- TASK/task2305/JavaOne.java
+++
@@ -1,8 +0,0 @@
-package task2305;
-
-public class JavaOne {
-	public static void main(String[] args)
-	{
-		System.out.println("Hellow Nikul!!");
-	}
-}
--- TASK/task2305/JavaTwo.java
+++
@@ -1,8 +0,0 @@
-package task2305;
-
-public class JavaTwo {
-	public static void main(String[] args)
-	{
-		System.out.println("Java");
-	}
-}
